bf5102557c7545e977cfabbd5c083bc655627e6b
seolhee313 <125417882+seolhee313@users.noreply.github.com>
2024-01-06 23:21:24 +0900

    2024.01.06

--- src/assets/js/section1.js
+++ src/assets/js/section1.js
@@ -1,3 +1,23 @@
+// import Lenis from '@studio-freight/lenis';
+
+// export function smooth() {
+//   const lenis = new Lenis({
+//     duration: 3,
+//     easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+//   });
+
+//   function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+//   }
+
+//   requestAnimationFrame(raf);
+
+//   lenis.on('scroll', e => {
+//     console.log(e);
+//   });
+// }
+
 window.onload = function () {
   measurePageLoadTime();
 };
